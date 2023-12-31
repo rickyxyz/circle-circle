0f2dce9381f0c4e04265c148c9259e36e1e126c7
rickyxyz <56119701+rickyxyz@users.noreply.github.com>
2023-12-31 13:59:57 +0700

    chore: upgrade vitest to v1.1.0

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
 
-import { defineConfig } from 'vite';
+import { UserConfig, defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react()] as UserConfig['plugins'],
   test: {
     globals: true,
     environment: 'jsdom',
