9d1d0312e8d55c24f79ce67c8a90e0ab7b36302c
rickyxyz <56119701+rickyxyz@users.noreply.github.com>
2024-01-13 15:20:36 +0700

    feat: add circle cache

--- src/context/AuthProvider.tsx
+++ src/context/AuthProvider.tsx
@@ -3,6 +3,8 @@ import { User } from '@/types/db';
 import { User as FirebaseUser } from 'firebase/auth';
 import { getCurrentUser } from '@/lib/firebase/auth';
 import { getData } from '@/lib/firebase/firestore';
+import { addCircles } from '@/redux/cacheReducer';
+import { useAppDispatch, useAppSelector } from '@/hook/reduxHooks';
 
 interface AuthContext {
   user: User | null;
@@ -18,12 +20,27 @@ export const AuthContext = createContext<AuthContext>({
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
   const [user, setUser] = useState<User | null>(null);
+  const circleCache = useAppSelector((state) => state.cache.circles);
+  const dispatch = useAppDispatch();
 
   const handleUserData = async (userAuth: FirebaseUser | null) => {
     if (userAuth) {
       const userData = await getData('user', userAuth.uid);
       if (userData) {
         setUser(userData);
+        userData.circle.forEach((circleId) => {
+          if (!Object.keys(circleCache).includes(circleId)) {
+            getData('circle', circleId)
+              .then((circle) => {
+                if (circle) {
+                  dispatch(addCircles([circle]));
+                } else throw new Error('user not found');
+              })
+              .catch((e) => {
+                throw e;
+              });
+          }
+        });
       }
     }
   };
@@ -34,6 +51,7 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
       .catch((e) => {
         throw e;
       });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
